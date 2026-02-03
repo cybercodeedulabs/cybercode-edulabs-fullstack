65eeae8b8e454079e1667bd83b0d7c29e23bb158
Cybercode <cybercodeedulabs@gmail.com>
2026-02-03 12:04:07 +0530

    removed render url

--- src/config/api.js
+++ src/config/api.js
@@ -1,4 +1,6 @@
 // src/config/api.js
-const API_URL = "https://cybercode-backend.onrender.com"; // Render backend URL
-
+const API_URL = import.meta.env.VITE_API_URL;
+if(!API_URL){
+    console.warn("[CONFIG WARNING] VITE_API_URL is not defined. API calls will fail");
+}
 export default API_URL;
